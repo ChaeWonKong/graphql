43d1f06a748819b9cf0d76c790ff5be57594bdd1
공채원 <chaewonkong@gmail.com>
2018-09-23 21:49:21 +0900

    Add hard-coded data array to schema and export schema

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,6 +1,11 @@
 const graphql = require("graphql");
+const _ = require("lodash");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const users = [
+  { id: "23", firstName: "Sanga", age: 26 },
+  { id: "47", firstName: "Leon", age: 27 }
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -9,4 +14,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
